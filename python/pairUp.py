c9b31619437287c556c2e33108f466da1a326d08
serine <tskiril@gmail.com>
2015-04-13 12:33:11 +1000

    check

---
+++ python/pairUp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+import sys
+
+for i in sys.arg[1]:
+    print i
---
+++ test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+FILES=$(ls ~/working_dir/cell-free-rna)
+
+for i in $FILES
+do
+  echo $i
+#  break
+done
