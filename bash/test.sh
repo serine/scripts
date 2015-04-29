1454852568c769c8e4dfebbbd25b23da02d03063
serine <tskiril@gmail.com>
2015-04-29 13:39:41 +1000

    imporved rnas-pipe.bds and makeTable.py

--- bash/test.sh
+++ bash/test.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
-FILES=$(ls ~/working_dir/cell-free-rna)
+CHECK="R1"
 
-for i in $FILES
+for i in "$@" 
 do
-  echo $i
-#  break
+  #echo $i
+  #if [[ $i =~ $CHECK ]]; then
+  #  echo $i
+  #fi
+  if [ "${i/$CHECK}" != "$i" ]; then
+    TEST="${i/%R1_001.fastq.gz/L001_R1.fastq.gz}"
+    #ln -s $i $TEST
+    ln -s $i $(basename $TEST)
+  else
+    TEST="${i/%R2_001.fastq.gz/L001_R2.fastq.gz}"
+    ln -s $i $(basename $TEST)
+    #ln -s $i $TEST
+  fi
 done
