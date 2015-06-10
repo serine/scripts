47b513d07347350316fdd613df466768e7f26276
serine <tskiril@gmail.com>
2015-06-10 23:23:39 +1000

    milestone! I got STAR functions working fine

--- python/chr.py
+++ python/chr.py
@@ -2,9 +2,22 @@
 
 import sys
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
+threshold = 0
 #chrom =">Chr%s"
-
-for seqRecord in SeqIO.parse(open(sys.argv[1]), 'fasta'):
-    print '\t'.join([seqRecord.id, str(len(seqRecord.seq))])
+bootstrap = 'length %s, coverage %s'
+for record in SeqIO.parse(open(sys.argv[1]), 'fasta'):
+    splitID = record.id.split("_")
+    coverage = float(splitID[-3])
+    if coverage > threshold:
+        name = ''.join((splitID[0], splitID[1]))
+        length = splitID[3]
+        coverage = splitID[5]
+        info = bootstrap % (length, coverage)
+        seqRecord = SeqRecord(seq=record.seq, id=name, description=info)
+        #print coverage, record.id
+    #print '\t'.join([seqRecord.id, str(len(seqRecord.seq))])
     #print seqRecord.id, len(seqRecord.seq)
+    #if seqRecord.id.startswith('NODE_2_length_84994'):
+        SeqIO.write(seqRecord, sys.stdout, 'fasta')
--- python/gffHack.py
+++ python/gffHack.py
@@ -1,68 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-15 -*-
+
 import sys, re
 
-f = open(sys.argv[1])
+geneHash = {}
+bootstrap = 'ID=%s;Note=%s;Name=%s'
+# pseudogenic_exon
 
-#for skip in range(5):
-#    f.next()
-for i in f:
-    line = i.strip().split('\t')
-    if line[2] == 'exon':
-         #geneName = re.search('ID=[A-Za-z0-9]*.\d*.\d*', line[8])
-         #print geneName.group(0)
-         newItem = line[8].split(';')[-1]
-         #secondBit = line[8].split('=')[-1]+'-Protein;'
-         #firstBit = line[8].split(';')[-1]
-         #newItem = firstBit+','+secondBit
-         line.pop()
-         line.append(newItem)
-    print '\t'.join(line)
-    #if len(i.strip().split("\t")) != 9:
-    #    print i
-    ##----------------------------------------------------------------------
-    ## Joe's hack
-    ##----------------------------------------------------------------------
-    #line = i.split("\t")
-    #itemTwo = line[1]
-    #columnTwo = line[1].split()
-    #firstAttr = columnTwo[0].replace("/", "_")
-    #secondAttr = columnTwo[1]
-    #thirdAttr = ''.join(columnTwo[-2].split("_"))
-    #fourthAttr = columnTwo[-1].strip("()")
-    #modifiedColumnTwo = '_'.join([firstAttr, secondAttr, thirdAttr, fourthAttr])
-    #line.remove(itemTwo)
-    #line.insert(1, modifiedColumnTwo)
-    #print '\t'.join(line).strip()
-    ##----------------------------------------------------------------------
+toInsert = ['miRNA', 
+            'other_RNA',
+            'protein_coding_gene',
+            'rRNA',
+            'snoRNA',
+            'tRNA',
+           ]
 
-#    #----------------------------------------------------------------------
-#    line = i.split("\t")
-#    attrs = line[8].strip().split(";")
-#    typeAttr = attrs[4]
-#    newType = typeAttr.replace('gene_biotype', 'transcript_type')
-#    attrs.insert(2, newType)
-#    newAttrs = ';'.join(attrs)
-#    #print i.replace(line[8], newAttrs).strip()
-#    print '\t'.join(line).strip()
-    #----------------------------------------------------------------------
-    #geneCheck = line[2]
-    #if geneCheck == 'gene':
-    #    #print line
-    #    columnNine = line[8]
-    #    attrs = columnNine.split(";")
-    #    geneIDAttr = attrs[0]
-    #    geneBiotypeAttr = attrs[-2]
+toCheck = ['CDS',
+           'exon',
+           'five_prime_UTR',
+           'gene',
+           'miRNA',
+           'mRNA',
+           'ncRNA',
+           'protein',
+           'pseudogene',
+           'pseudogenic_transcript',
+           'rRNA',
+           'snoRNA',
+           'three_prime_UTR',
+           'transposable_element_gene',
+           'tRNA',
+          ]
+features = []
+gene = None
 
-    #    transcriptIDAttr = geneIDAttr.replace('gene_id', ' transcript_id')
-    #    transcriptBiotypeAttr = geneBiotypeAttr.replace('gene_biotype', 'transcript_biotype')
+for i in open(sys.argv[1]):
+    line = i.split()
+    if line[2].strip() == 'gene':
+        if features:
+            #print gene
+            print features[0]
+            #print '----------'
 
-    #    attrs.insert(1, transcriptIDAttr)
-    #    attrs.insert(-2, transcriptBiotypeAttr)
-    #    newAttrs = ';'.join(attrs)
-    #    line.pop()
-    #    line.insert(-1, newAttrs)
-    #    line.pop()
-    #    print "\t".join(line).strip()
-    #else:
-    #    print "\t".join(line).strip()
+        features = []
+        gene = line
+    else:
+        features.append(i.strip())
+#for i in open(sys.argv[1]):
+#   line = i.strip().split("\t")
+#   m = re.search(':([A-Z0-9]+)', line[8])
+#   key = m.group(1) 
+#   #value = '\t'.join([line[0], line[-1]])
+#   value = i
+#
+#   if key not in geneHash:
+#       geneHash[key] = []
+#
+#   geneHash[key].append(value)
+#   #print key, value
+#   #print line[-1]
+#
+##for key, value in geneHash.items(): #.values():
+##  print key, value
+#for key, value in geneHash.items():
+#   for i in value:
+#       m = re.search('mRNA', i)
+#       if m:
+#           print m.group(0)
+#       #m = re.search('mRNA', value)
+#   #print m.group(0)
+#   break
+    #for i in value:
+    #   print i.strip()
+    #   line = i.split("\t")
+    #   if line[0] in toCheck:
+    #       #getIndex = check.index(i)
+    #       if i.startswith(i):
+    #           print bootstrap % (key, 'protein_coding_gene', key)
+    #   print line[0]
+    #break
