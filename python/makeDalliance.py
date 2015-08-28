7ff132d82a515587edae26e96438f5874ff0c3af
serine <tskiril@gmail.com>
2015-08-28 10:47:49 +1000

    random

--- python/makeDalliance.py
+++ python/makeDalliance.py
@@ -15,9 +15,7 @@ htmlTemplate = '''
         maxHeight: 900,  // maximum hight in CSS pixels 
         pageName:     'mouseTwo-holder', // Target element ID
         //----------------------------------------------------------------------------------------------------
-        chr:          '1', // Use the same name you have in your .2bit file
-        viewStart:    56717201,
-        viewEnd:      56815472,
+        %s
     
         coordSystem: {
           speciesName: 'Mus musculus',
@@ -32,13 +30,12 @@ htmlTemplate = '''
                        desc:        'There will be longer desctiption placed here',
                        twoBitURI:   '%s'
                       },
-                      {name:        'Annoatation',
+                      {name:        'Annotation',
                        desc:        'Genes annotation, showing Exons and Introns boundaries',
                        bwgURI:      '%s',
                        collapseSuperGroups: true,
                        trixURI:     '%s',
-                      },
-                      %s
+                      }, %s
                      ],
       });
     </script>
@@ -49,14 +46,20 @@ htmlTemplate = '''
 </html>   
 '''
 
-bamString = '''
-{name:      'sample standard',
+coordinatesTemplate = '''
+        chr:      '%s', // Use the same name you have in your .2bit file
+        viewStart: %s,
+        viewEnd:   %s,
+'''
+
+bamTrackTemplate = '''
+                       {name:       '%s',
                        disabled:     true, // This is "untick" track
                        bamURI:      '%s',
                        baiURI:      '%s.bai',
-                       style: [{type: "density",
+                       style: [
+                               {type: "density",
                                 zoom: "low",
-
                                 style: {glyph: "HISTOGRAM",
                                         COLOR1: "black",
                                         COLOR2: "red",
@@ -83,21 +86,44 @@ bamString = '''
                                         __SEQCOLOR: "mismatch"
                                        }
                                },
-
                               ],
                        },
 
 '''
-check = 'check'
-#print bamString % (check, check)
 
-import sys, os
+import sys, os, argparse
 
-refArg = sys.argv[1]
-dataArg = sys.argv[1]
+parser = argparse.ArgumentParser(usage='%(prog)s --dataDirectory <path/to/yourData> --referenceFiles <path/to/yourReferenceFiles>',
+                                 description='This scripts makes BioDalliance webpage',
+                                 )
+parser.add_argument('--dataDirectory',
+                    required=True,
+                    help="specify directory with data files to be made into BioDalliance tracks"
+                    )
+parser.add_argument('--referenceFiles',
+                    required=True,
+                    help='specify directory with reference files e.g refFile.2bit and refAnnotation.bb'
+                    )             
+parser.add_argument('--defaultCoordinates',
+                    nargs=3,
+                    default=[12, 24565477, 24624103],
+                    help="This will be your default coordinates for BioDallaince browser.\
+                          Pass coordinates as follows <Chromosome Start End>"
+                    )
+parser.add_argument('--serverName',
+                    default='bioinformatics.erc.monash.edu'
+                    )
 
-refFiles = os.listdir(refArg)
-dataFiles = os.listdir(dataArg)
+args = parser.parse_args()
+dataDirectory = args.dataDirectory
+referenceFiles = args.referenceFiles
+defaultCoordinates = args.defaultCoordinates
+serverName = args.serverName
+
+coordinates = coordinatesTemplate % tuple(defaultCoordinates)
+
+refFiles = os.listdir(referenceFiles)
+dataFiles = os.listdir(dataDirectory)
 
 genome = ''
 annotation = ''
@@ -105,10 +131,20 @@ index = ''
 
 for i in refFiles:
     if i.endswith('.2bit'):
-        genome = os.path.join(refArg, i)
+        genome = os.path.join(referenceFiles, i)
     if i.endswith('bb'):
-        annotation = os.path.join(refArg, i)
+        annotation = os.path.join(referenceFiles, i)
     if i.endswith('ix'):
-        index = os.path.join(refArg, i)
+        index = os.path.join(referenceFiles, i)
 
-print genome, annotation, index
+bamTracks = []
+
+for bamFile in dataFiles:
+    if bamFile.endswith('sorted.bam'):
+        name = bamFile.split('_')[0]
+        bam = os.path.join(dataDirectory, bamFile)
+        bamTrack = bamTrackTemplate % (name, bam, bam)
+        bamTracks.append(bamTrack)
+
+#print htmlTemplate % (genome, annotation, index, [i.strip() for i in bamTracks])
+print htmlTemplate % (coordinates, genome, annotation, index, ' '.join(bamTracks))
